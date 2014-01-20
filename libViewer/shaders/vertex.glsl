ebbee688ae70719bc93b5e995de38c6cc30523ba
Pelle Johnsen <pelle.johnsen@gmail.com>
2014-01-20 15:24:39 +0100

    get shader files back

--- libViewer/shaders/vertex.glsl
+++ libViewer/shaders/vertex.glsl
@@ -1,7 +1,12 @@
+#ifdef GL_ES
+// Set default precision to medium
+precision mediump int;
+precision mediump float;
+#endif
+
 attribute vec4 vPosition;
 
 void main(void)
 {
     gl_Position = vPosition;
 }
-
