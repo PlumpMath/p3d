c92c655242271a538ae2b2b2258d79f4c25f9b05
Nathan Letwory <nathan@letworyinteractive.com>
2014-08-05 13:43:11 +0300

    Spaces -> tabs

--- libViewer/BlendLoader.cpp
+++ libViewer/BlendLoader.cpp
@@ -7,13 +7,13 @@ static RegisterLoader registerBlendLoader(&blendLoader, ".blend", 0);
 
 BlendLoader::BlendLoader()
 {
-    m_loaded = false;
-    m_minX = FLT_MAX;
-    m_maxX = FLT_MIN;
-    m_minY = FLT_MAX;
-    m_maxY = FLT_MIN;
-    m_minZ = FLT_MAX;
-    m_maxZ = FLT_MIN;
+	m_loaded = false;
+	m_minX = FLT_MAX;
+	m_maxX = FLT_MIN;
+	m_minY = FLT_MAX;
+	m_maxY = FLT_MIN;
+	m_minZ = FLT_MAX;
+	m_maxZ = FLT_MIN;
 }
 
 BlendLoader::~BlendLoader()
@@ -24,276 +24,276 @@ BlendLoader::~BlendLoader()
 
 bool BlendLoader::load(const char *data, size_t length)
 {
-    P3dConverter converter;
-    BlendData blendData;
+	P3dConverter converter;
+	BlendData blendData;
 
-    m_modelLoader->clear();
-    m_chunks.clear();
+	m_modelLoader->clear();
+	m_chunks.clear();
 
-    converter.parse_blend(data, length);
-    blendData.initBlendData(converter);
+	converter.parse_blend(data, length);
+	blendData.initBlendData(converter);
 
-    uint64_t start = PlatformAdapter::currentMillis();
-    uint32_t chunk = 0;
+	uint64_t start = PlatformAdapter::currentMillis();
+	uint32_t chunk = 0;
 
-    uint16_t* new_faces = new uint16_t[blendData.totface*STRIDE];
+	uint16_t* new_faces = new uint16_t[blendData.totface*STRIDE];
 
-    /* initialize counters and indices */
-    m_new_pos_count = 0;
-    m_new_norm_count = 0;
-    m_new_uv_count = 0;
-    m_total_index_count = 0;
+	/* initialize counters and indices */
+	m_new_pos_count = 0;
+	m_new_norm_count = 0;
+	m_new_uv_count = 0;
+	m_total_index_count = 0;
 
-    for(int vtype = 0; vtype < 4; vtype++)
-    {
-        m_new_index_count[vtype] = 0;
-        m_new_f3_start[vtype] = 0;
-        m_new_f4_start[vtype] = 0;
-    }
+	for(int vtype = 0; vtype < 4; vtype++)
+	{
+		m_new_index_count[vtype] = 0;
+		m_new_f3_start[vtype] = 0;
+		m_new_f4_start[vtype] = 0;
+	}
 
-    /* for now we do only VT_POS */
-    m_new_index_count[VT_POS] = blendData.totface * 3;
-    m_new_f3_start[VT_POS] = 0;
-    m_new_f4_start[VT_POS] = -1;
-    m_total_index_count += m_new_index_count[VT_POS];
+	/* for now we do only VT_POS */
+	m_new_index_count[VT_POS] = blendData.totface * 3;
+	m_new_f3_start[VT_POS] = 0;
+	m_new_f4_start[VT_POS] = -1;
+	m_total_index_count += m_new_index_count[VT_POS];
 
-    /* reindex VT_POS */
-    reindexType(chunk, VT_POS, &blendData, new_faces);
+	/* reindex VT_POS */
+	reindexType(chunk, VT_POS, &blendData, new_faces);
 
-    GLfloat* new_pos = new GLfloat[blendData.totvert * STRIDE];
-    GLfloat* new_uv = new GLfloat[blendData.totvert * STRIDE];
-    GLfloat* new_norm = new GLfloat[blendData.totvert * STRIDE];
+	GLfloat* new_pos = new GLfloat[blendData.totvert * STRIDE];
+	GLfloat* new_uv = new GLfloat[blendData.totvert * STRIDE];
+	GLfloat* new_norm = new GLfloat[blendData.totvert * STRIDE];
 
-    if(new_pos==NULL || new_uv==NULL || new_norm==NULL) return false;
+	if(new_pos==NULL || new_uv==NULL || new_norm==NULL) return false;
 
-    P3D_LOGD("GLfloat buffers allocated");
+	P3D_LOGD("GLfloat buffers allocated");
 
-    for(chunk = 0; chunk < m_chunks.size(); ++chunk)
-    {
-        P3D_LOGD("chunk: %d", chunk);
-        P3D_LOGD(" index count: %d", m_chunks[chunk].indexCount);
-        P3D_LOGD(" vert count: %d", m_chunks[chunk].vertCount);
-        P3D_LOGD(" f3 offset: %d", m_chunks[chunk].f3Offset);
-        P3D_LOGD(" f4 offset: %d", m_chunks[chunk].f4Offset);
-        P3D_LOGD(" material: %d", m_chunks[chunk].material);
-    }
+	for(chunk = 0; chunk < m_chunks.size(); ++chunk)
+	{
+		P3D_LOGD("chunk: %d", chunk);
+		P3D_LOGD(" index count: %d", m_chunks[chunk].indexCount);
+		P3D_LOGD(" vert count: %d", m_chunks[chunk].vertCount);
+		P3D_LOGD(" f3 offset: %d", m_chunks[chunk].f3Offset);
+		P3D_LOGD(" f4 offset: %d", m_chunks[chunk].f4Offset);
+		P3D_LOGD(" material: %d", m_chunks[chunk].material);
+	}
 
-    P3D_LOGD("----------");
+	P3D_LOGD("----------");
 
-    for(auto item: m_vertex_maps)
-    {
-        P3D_LOGD("vertex bank:");
-        P3D_LOGD(" offset: %d", item.first);
-        P3D_LOGD(" count: %d", item.second->size());
-        copyVertData(item.first, item.second, blendData, new_norm, new_uv, new_pos);
-        item.second->dumpBucketLoad();
-        delete item.second;
-    }
+	for(auto item: m_vertex_maps)
+	{
+		P3D_LOGD("vertex bank:");
+		P3D_LOGD(" offset: %d", item.first);
+		P3D_LOGD(" count: %d", item.second->size());
+		copyVertData(item.first, item.second, blendData, new_norm, new_uv, new_pos);
+		item.second->dumpBucketLoad();
+		delete item.second;
+	}
 
-    P3D_LOGD("data copied");
-    m_vertex_maps.clear();
+	P3D_LOGD("data copied");
+	m_vertex_maps.clear();
 
-    m_modelLoader->createModel(m_new_pos_count, m_new_norm_count, m_new_empty_norm_count, m_new_uv_count,
-                new_pos, new_norm, new_uv, m_total_index_count,
-                new_faces, m_chunks.size(), m_chunks.data());
+	m_modelLoader->createModel(m_new_pos_count, m_new_norm_count, m_new_empty_norm_count, m_new_uv_count,
+				new_pos, new_norm, new_uv, m_total_index_count,
+				new_faces, m_chunks.size(), m_chunks.data());
 
-    delete [] new_norm;
-    delete [] new_uv;
-    delete [] new_pos;
+	delete [] new_norm;
+	delete [] new_uv;
+	delete [] new_pos;
 
-    delete [] new_faces;
+	delete [] new_faces;
 
-    m_loaded = true;
+	m_loaded = true;
 
-    m_modelLoader->setBoundingBox(m_minX, m_maxX, m_minY, m_maxY, m_minZ, m_maxZ);
-    m_modelLoader->setIsLoaded(m_loaded);
+	m_modelLoader->setBoundingBox(m_minX, m_maxX, m_minY, m_maxY, m_minZ, m_maxZ);
+	m_modelLoader->setIsLoaded(m_loaded);
 
-    P3D_LOGD("reindex took %lldms", PlatformAdapter::durationMillis(start));
+	P3D_LOGD("reindex took %lldms", PlatformAdapter::durationMillis(start));
 
-    return m_loaded;
+	return m_loaded;
 }
 
 uint32_t BlendLoader::reindexType(uint32_t &chunk, BlendLoader::VertexType vtype, const BlendData *blendData,
-                                  uint16_t* new_faces)
+								  uint16_t* new_faces)
 {
-    uint32_t pos_offset;
-    uint16_t mat = 0;
-    uint32_t face;
-    uint32_t fcount;
-    uint32_t vert;
-    uint32_t verts;
-    uint32_t new_offset;
+	uint32_t pos_offset;
+	uint16_t mat = 0;
+	uint32_t face;
+	uint32_t fcount;
+	uint32_t vert;
+	uint32_t verts;
+	uint32_t new_offset;
 
-    uint64_t start = PlatformAdapter::currentMillis();
+	uint64_t start = PlatformAdapter::currentMillis();
 
-    uint32_t result = 0;
-    P3dMap<VertexIndex, uint32_t>* vertexMap = 0;
-    bool in_quad = false;
+	uint32_t result = 0;
+	P3dMap<VertexIndex, uint32_t>* vertexMap = 0;
+	bool in_quad = false;
 
-    fcount = blendData->totface;
+	fcount = blendData->totface;
 
-    if(fcount == 0)
-    {
-        // no faces of this type
-        return result;
-    }
+	if(fcount == 0)
+	{
+		// no faces of this type
+		return result;
+	}
 
-    // tris
-    pos_offset = 0;
+	// tris
+	pos_offset = 0;
 
-    VertexIndex index;
-    index.type = vtype;
-    uint16_t new_index;
-    new_offset = 0; //TODO: should be passed in?
+	VertexIndex index;
+	index.type = vtype;
+	uint16_t new_index;
+	new_offset = 0; //TODO: should be passed in?
 
-    for(face = 0; face < fcount; ++face)
-    {
-        if(face == 0)
-        {
-            nextChunk(chunk, vtype, in_quad, new_offset, m_new_pos_count / 3, true);
-            m_chunks[chunk].material = mat;
-            vertexMap = m_vertex_maps[m_chunks[chunk].vertOffset];
-        }
+	for(face = 0; face < fcount; ++face)
+	{
+		if(face == 0)
+		{
+			nextChunk(chunk, vtype, in_quad, new_offset, m_new_pos_count / 3, true);
+			m_chunks[chunk].material = mat;
+			vertexMap = m_vertex_maps[m_chunks[chunk].vertOffset];
+		}
 
-        verts = in_quad ? 4 : 3;
-        for(vert = 0; vert < verts; ++vert)
-        {
-            index.pos = blendData->faces[pos_offset];
-            pos_offset++;
+		verts = in_quad ? 4 : 3;
+		for(vert = 0; vert < verts; ++vert)
+		{
+			index.pos = blendData->faces[pos_offset];
+			pos_offset++;
 
-            /* TODO: norm */
-            index.norm = 0;
-            index.uv = 0;
+			/* TODO: norm */
+			index.norm = 0;
+			index.uv = 0;
 
-            if(vertexMap->count(index))
-            {
-                new_index = (*vertexMap)[index];
-            }
-            else
-            {
-                new_index = vertexMap->size();
-                vertexMap->insert(index, new_index);
+			if(vertexMap->count(index))
+			{
+				new_index = (*vertexMap)[index];
+			}
+			else
+			{
+				new_index = vertexMap->size();
+				vertexMap->insert(index, new_index);
 
-                m_new_pos_count += 3;
+				m_new_pos_count += 3;
 
-                m_new_norm_count += 3;
-                m_new_empty_norm_count += 3;
-            }
+				m_new_norm_count += 3;
+				m_new_empty_norm_count += 3;
+			}
 
-            new_faces[new_offset] = (uint16_t)new_index;
-            ++new_offset;
-        }
-    }
+			new_faces[new_offset] = (uint16_t)new_index;
+			++new_offset;
+		}
+	}
 
-    m_chunks[chunk].vertCount = (m_new_pos_count - STRIDE * m_chunks[chunk].vertOffset) / 3;
+	m_chunks[chunk].vertCount = (m_new_pos_count - STRIDE * m_chunks[chunk].vertOffset) / 3;
 
-    P3D_LOGD("reindex type Blender took: %lldms", PlatformAdapter::durationMillis(start));
+	P3D_LOGD("reindex type Blender took: %lldms", PlatformAdapter::durationMillis(start));
 
-    return result;
+	return result;
 }
 
 void BlendLoader::nextChunk(uint32_t &chunk, BlendLoader::VertexType vtype, bool in_f4, uint32_t new_offset,
-                            uint32_t vertOffset, bool firstOfType)
+							uint32_t vertOffset, bool firstOfType)
 {
-    P3D_LOGD("new_offset: %d", new_offset);
-    P3D_LOGD("vertOffset: %d", vertOffset);
-    if(m_chunks.size() != 0)
-    {
-        ++chunk;
-    }
-    if(chunk >= m_chunks.size())
-    {
-        while(chunk >= m_chunks.size()) m_chunks.push_back(MeshChunk());
-    }
-    else
-    {
-        m_chunks[chunk] = MeshChunk();
-    }
-    m_chunks[chunk] = MeshChunk();
-    MeshChunk& newChunk = m_chunks[chunk];
-    newChunk.indexCount = m_new_index_count[vtype];
-    newChunk.f3Offset = new_offset;
+	P3D_LOGD("new_offset: %d", new_offset);
+	P3D_LOGD("vertOffset: %d", vertOffset);
+	if(m_chunks.size() != 0)
+	{
+		++chunk;
+	}
+	if(chunk >= m_chunks.size())
+	{
+		while(chunk >= m_chunks.size()) m_chunks.push_back(MeshChunk());
+	}
+	else
+	{
+		m_chunks[chunk] = MeshChunk();
+	}
+	m_chunks[chunk] = MeshChunk();
+	MeshChunk& newChunk = m_chunks[chunk];
+	newChunk.indexCount = m_new_index_count[vtype];
+	newChunk.f3Offset = new_offset;
 
-    newChunk.validNormals = false;
-    newChunk.hasUvs = false;
+	newChunk.validNormals = false;
+	newChunk.hasUvs = false;
 
-    newChunk.vertOffset = vertOffset;
+	newChunk.vertOffset = vertOffset;
 
-    if(firstOfType)
-    {
-        newChunk.indexCount = m_new_index_count[vtype];
-        newChunk.f3Offset = m_new_f3_start[vtype];
-        newChunk.f4Offset = m_new_f4_start[vtype];
-    }
-    /*else
-    {
-        MeshChunk& oldChunk = m_chunks[chunk - 1];
-        if(!in_f4)
-        {
-            newChunk.f4Offset = oldChunk.f4Offset - new_offset;
-        }
-        newChunk.indexCount = oldChunk.indexCount - (new_offset - oldChunk.f3Offset);
-        oldChunk.indexCount = new_offset - oldChunk.f3Offset;
-        oldChunk.vertCount = (m_new_pos_count - oldChunk.vertOffset * 3) / 3;
-    }*/
+	if(firstOfType)
+	{
+		newChunk.indexCount = m_new_index_count[vtype];
+		newChunk.f3Offset = m_new_f3_start[vtype];
+		newChunk.f4Offset = m_new_f4_start[vtype];
+	}
+	/*else
+	{
+		MeshChunk& oldChunk = m_chunks[chunk - 1];
+		if(!in_f4)
+		{
+			newChunk.f4Offset = oldChunk.f4Offset - new_offset;
+		}
+		newChunk.indexCount = oldChunk.indexCount - (new_offset - oldChunk.f3Offset);
+		oldChunk.indexCount = new_offset - oldChunk.f3Offset;
+		oldChunk.vertCount = (m_new_pos_count - oldChunk.vertOffset * 3) / 3;
+	}*/
 
-    if(m_vertex_maps.count(newChunk.vertOffset) == 0)
-    {
-        m_vertex_maps[newChunk.vertOffset] = new P3dMap<VertexIndex, uint32_t>(8192);
-    }
+	if(m_vertex_maps.count(newChunk.vertOffset) == 0)
+	{
+		m_vertex_maps[newChunk.vertOffset] = new P3dMap<VertexIndex, uint32_t>(8192);
+	}
 }
 
 
 
 void BlendLoader::copyVertData(uint32_t vertOffset, P3dMap<VertexIndex, uint32_t>* vertexMap, const BlendData& data,
-                               GLfloat* new_norm, GLfloat* new_uv, GLfloat* new_pos)
+							   GLfloat* new_norm, GLfloat* new_uv, GLfloat* new_pos)
 {
-    uint32_t new_offset = 0;
-    uint32_t vert_offset = vertOffset;
-    float x;
-    float y;
-    float z;
-    unsigned int vertCount = 0;
-    P3D_LOGD("vert offset: %u", vertOffset);
-    for(auto item: *vertexMap)
-    {
-        ++vertCount;
-        const VertexIndex& index = item.first;
-        uint32_t new_index = item.second;
-        //P3D_LOGD("%u: %u/%u/%u > %u", vertCount, index.pos, index.uv, index.norm, new_index);
+	uint32_t new_offset = 0;
+	uint32_t vert_offset = vertOffset;
+	float x;
+	float y;
+	float z;
+	unsigned int vertCount = 0;
+	P3D_LOGD("vert offset: %u", vertOffset);
+	for(auto item: *vertexMap)
+	{
+		++vertCount;
+		const VertexIndex& index = item.first;
+		uint32_t new_index = item.second;
+		//P3D_LOGD("%u: %u/%u/%u > %u", vertCount, index.pos, index.uv, index.norm, new_index);
 
-        // pos
-        vert_offset = index.pos * STRIDE;
-        new_offset = (new_index + vertOffset) * STRIDE;
+		// pos
+		vert_offset = index.pos * STRIDE;
+		new_offset = (new_index + vertOffset) * STRIDE;
 
-        x = data.verts[vert_offset++];
-        y = data.verts[vert_offset++];
-        z = data.verts[vert_offset];
+		x = data.verts[vert_offset++];
+		y = data.verts[vert_offset++];
+		z = data.verts[vert_offset];
 
-        //P3D_LOGD("%u @ %u: (%f,%f,%f)", vertCount, vert_offset, x, y, z);
+		//P3D_LOGD("%u @ %u: (%f,%f,%f)", vertCount, vert_offset, x, y, z);
 
-        if(x > m_maxX) m_maxX = x;
-        if(x < m_minX) m_minX = x;
-        if(y > m_maxY) m_maxY = y;
-        if(y < m_minY) m_minY = y;
-        if(z > m_maxZ) m_maxZ = z;
-        if(z < m_minZ) m_minZ = z;
-        new_pos[new_offset++] = x;
-        new_pos[new_offset++] = y;
-        new_pos[new_offset] = z;
+		if(x > m_maxX) m_maxX = x;
+		if(x < m_minX) m_minX = x;
+		if(y > m_maxY) m_maxY = y;
+		if(y < m_minY) m_minY = y;
+		if(z > m_maxZ) m_maxZ = z;
+		if(z < m_minZ) m_minZ = z;
+		new_pos[new_offset++] = x;
+		new_pos[new_offset++] = y;
+		new_pos[new_offset] = z;
 
-        // norm
-        vert_offset = index.pos * STRIDE;
-        new_offset = (new_index + vertOffset) * STRIDE;
-        // store empty normal
-        // TODO: actual normal storage if present in BlendData
-        new_norm[new_offset] = .5f;
-        new_uv[new_offset++] = 1.0f;
-        new_norm[new_offset] = .5f;
-        new_uv[new_offset++] = 1.0f;
-        new_norm[new_offset] = .5f;
-        new_uv[new_offset] = 1.0f;
-    }
+		// norm
+		vert_offset = index.pos * STRIDE;
+		new_offset = (new_index + vertOffset) * STRIDE;
+		// store empty normal
+		// TODO: actual normal storage if present in BlendData
+		new_norm[new_offset] = .5f;
+		new_uv[new_offset++] = 1.0f;
+		new_norm[new_offset] = .5f;
+		new_uv[new_offset++] = 1.0f;
+		new_norm[new_offset] = .5f;
+		new_uv[new_offset] = 1.0f;
+	}
 
-    P3D_LOGD("BB: %f:%f %f:%f %f:%f", m_minX, m_maxX, m_minY, m_maxY, m_minZ, m_maxZ);
+	P3D_LOGD("BB: %f:%f %f:%f %f:%f", m_minX, m_maxX, m_minY, m_maxY, m_minZ, m_maxZ);
 }
--- libViewer/BlendLoader.h
+++ libViewer/BlendLoader.h
@@ -16,149 +16,149 @@
 /** Class to pass around data buffers from Blender files */
 class BlendData {
 public:
-    BlendData()
-    {
-        totvert = 0;
-        totface = 0;
-        vertbytes = 0;
-        facebytes = 0;
+	BlendData()
+	{
+		totvert = 0;
+		totface = 0;
+		vertbytes = 0;
+		facebytes = 0;
 		verts = nullptr;
 		faces = nullptr;
-        isloaded = false;
-    }
-    ~BlendData()
-    {
-        clearBlendData();
-    }
+		isloaded = false;
+	}
+	~BlendData()
+	{
+		clearBlendData();
+	}
 
-    /** Data is expected to be triangulated before being passed in here. */
-    void initBlendData(uint32_t tv, uint32_t tf, float *vs, uint32_t *fs) {
-        isloaded = false;
-        if(verts != 0) delete [] verts;
-        if(faces != 0) delete [] faces;
+	/** Data is expected to be triangulated before being passed in here. */
+	void initBlendData(uint32_t tv, uint32_t tf, float *vs, uint32_t *fs) {
+		isloaded = false;
+		if(verts != 0) delete [] verts;
+		if(faces != 0) delete [] faces;
 
-        totvert = tv;
-        totface = tf;
+		totvert = tv;
+		totface = tf;
 
-        vertbytes = totvert * sizeof(float) * STRIDE;
-        facebytes = totface * sizeof(uint32_t) * STRIDE;
+		vertbytes = totvert * sizeof(float) * STRIDE;
+		facebytes = totface * sizeof(uint32_t) * STRIDE;
 
-        verts = new float[totvert*STRIDE];
-        faces = new uint32_t[totface*STRIDE];
+		verts = new float[totvert*STRIDE];
+		faces = new uint32_t[totface*STRIDE];
 
-        float *v, *vnew;
-        uint32_t *f, *fnew;
-        unsigned int i;
-        for(i=0, v = vs, vnew = verts; i < tv*STRIDE; i++, v++, vnew++) {
-            *vnew = *v;
-        }
+		float *v, *vnew;
+		uint32_t *f, *fnew;
+		unsigned int i;
+		for(i=0, v = vs, vnew = verts; i < tv*STRIDE; i++, v++, vnew++) {
+			*vnew = *v;
+		}
 
-        for(i=0, f = fs, fnew = faces; i < tf*STRIDE; i++, f++, fnew++) {
-            *fnew = *f;
-        }
+		for(i=0, f = fs, fnew = faces; i < tf*STRIDE; i++, f++, fnew++) {
+			*fnew = *f;
+		}
 
-        if(totvert>0 && verts!=NULL) {
-            isloaded = true;
-        } else {
-            clearBlendData();
-        }
-    }
+		if(totvert>0 && verts!=NULL) {
+			isloaded = true;
+		} else {
+			clearBlendData();
+		}
+	}
 
-    void initBlendData(P3dConverter &converter){
-        P3D_LOGD("Adding %u meshes", converter.object_count());
-        for(uint32_t i = 0; i < converter.object_count(); i++) {
-            P3dMesh &mesh = converter[0];
-            for(uint32_t j = 0; j < mesh.m_totchunk; j++) {
-                Chunk &chunk = mesh.m_chunks[j];
-                initBlendData(chunk.totvert, chunk.totface, chunk.v, chunk.f);
-            }
-        }
-    }
+	void initBlendData(P3dConverter &converter){
+		P3D_LOGD("Adding %u meshes", converter.object_count());
+		for(uint32_t i = 0; i < converter.object_count(); i++) {
+			P3dMesh &mesh = converter[0];
+			for(uint32_t j = 0; j < mesh.m_totchunk; j++) {
+				Chunk &chunk = mesh.m_chunks[j];
+				initBlendData(chunk.totvert, chunk.totface, chunk.v, chunk.f);
+			}
+		}
+	}
 
-    void clearBlendData() {
-        isloaded = false;
-        delete [] verts;
-        delete [] faces;
-        verts = 0;
-        faces = 0;
-        totvert = 0;
-        totface = 0;
-        vertbytes = 0;
-        facebytes = 0;
-    }
+	void clearBlendData() {
+		isloaded = false;
+		delete [] verts;
+		delete [] faces;
+		verts = 0;
+		faces = 0;
+		totvert = 0;
+		totface = 0;
+		vertbytes = 0;
+		facebytes = 0;
+	}
 
-    size_t size() const { return vertbytes + facebytes; }
+	size_t size() const { return vertbytes + facebytes; }
 
-    bool isLoaded() { return isloaded; }
+	bool isLoaded() { return isloaded; }
 
-    uint32_t totvert;
-    float *verts;
-    size_t vertbytes;
+	uint32_t totvert;
+	float *verts;
+	size_t vertbytes;
 
-    uint32_t totface;
-    uint32_t *faces;
-    size_t facebytes;
+	uint32_t totface;
+	uint32_t *faces;
+	size_t facebytes;
 
-    bool isloaded;
+	bool isloaded;
 };
 
 class BlendLoader : public BaseLoader
 {
 public:
-    BlendLoader();
-    virtual ~BlendLoader();
+	BlendLoader();
+	virtual ~BlendLoader();
 
-    bool load(const char *data, size_t length);
+	bool load(const char *data, size_t length);
 
 private:
-    uint32_t reindexType(uint32_t &chunk, VertexType vtype, const BlendData *blendData,
-                         uint16_t *new_faces);
-    void copyVertData(uint32_t vertOffset, P3dMap<VertexIndex, uint32_t>* vertexMap, const BlendData& data,
-                      GLfloat* new_norm, GLfloat* new_uv, GLfloat* new_pos);
-    void nextChunk(uint32_t &chunk, BaseLoader::VertexType vtype, bool in_f4, uint32_t new_offset,
-                   uint32_t vertOffset, bool firstOfType = false);
+	uint32_t reindexType(uint32_t &chunk, VertexType vtype, const BlendData *blendData,
+						 uint16_t *new_faces);
+	void copyVertData(uint32_t vertOffset, P3dMap<VertexIndex, uint32_t>* vertexMap, const BlendData& data,
+					  GLfloat* new_norm, GLfloat* new_uv, GLfloat* new_pos);
+	void nextChunk(uint32_t &chunk, BaseLoader::VertexType vtype, bool in_f4, uint32_t new_offset,
+				   uint32_t vertOffset, bool firstOfType = false);
 
-    bool m_loaded;
+	bool m_loaded;
 
-    uint32_t m_pos_count;
-    uint32_t m_norm_count;
-    uint32_t m_tex_count;
+	uint32_t m_pos_count;
+	uint32_t m_norm_count;
+	uint32_t m_tex_count;
 
-    uint32_t m_f3_count[4];
-    uint32_t m_f4_count[4];
+	uint32_t m_f3_count[4];
+	uint32_t m_f4_count[4];
 
-    uint32_t m_pos_start;
-    uint32_t m_norm_start;
-    uint32_t m_tex_start;
+	uint32_t m_pos_start;
+	uint32_t m_norm_start;
+	uint32_t m_tex_start;
 
-    uint32_t m_f3_start[4];
-    uint32_t m_f4_start[4];
+	uint32_t m_f3_start[4];
+	uint32_t m_f4_start[4];
 
-    uint16_t m_mat_count;
+	uint16_t m_mat_count;
 
-    // bounding box
-    float m_maxX;
-    float m_minX;
-    float m_maxY;
-    float m_minY;
-    float m_maxZ;
-    float m_minZ;
+	// bounding box
+	float m_maxX;
+	float m_minX;
+	float m_maxY;
+	float m_minY;
+	float m_maxZ;
+	float m_minZ;
 
-    // new data
-    P3dVector<MeshChunk> m_chunks;
+	// new data
+	P3dVector<MeshChunk> m_chunks;
 
-    P3dMap<uint32_t, P3dMap<VertexIndex, uint32_t>*> m_vertex_maps;
+	P3dMap<uint32_t, P3dMap<VertexIndex, uint32_t>*> m_vertex_maps;
 
-    uint32_t m_new_index_count[4];
-    uint32_t m_new_f3_start[4];
-    uint32_t m_new_f4_start[4];
+	uint32_t m_new_index_count[4];
+	uint32_t m_new_f3_start[4];
+	uint32_t m_new_f4_start[4];
 
-    size_t m_total_index_count;
+	size_t m_total_index_count;
 
-    uint32_t m_new_pos_count;
-    uint32_t m_new_norm_count;
-    uint32_t m_new_empty_norm_count;
-    uint32_t m_new_uv_count;
+	uint32_t m_new_pos_count;
+	uint32_t m_new_norm_count;
+	uint32_t m_new_empty_norm_count;
+	uint32_t m_new_uv_count;
 };
 
 #endif // BLENDLOADER_H
