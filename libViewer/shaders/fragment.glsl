a7a7ccd76fbb403d4f13379092b3f0dc899a458c
Pelle Johnsen <pelle.johnsen@gmail.com>
2014-08-06 15:36:23 +0200

    Shader fixes

--- libViewer/shaders/fragment.glsl
+++ libViewer/shaders/fragment.glsl
@@ -20,15 +20,15 @@ void main(void)
     
     vec3 lightDir1 = normalize(vec3(1.0, 1.0, 1.0));
     vec3 lightDir2 = normalize(vec3(-1.0, -1.0, -1.0));
-    vec3 diffColor = uDiffuseColor;
+    vec4 diffColor = vec4(uDiffuseColor, 1.0);
 #ifdef USE_DIFFUSE_TEXTURE
     if(enableDiffuse)
     {
         diffColor *= texture2D(tDiffuse, vUv);
     }
 #endif
-    vec3 diffuse = diffColor * max(0.0, dot(normal, lightDir1));
-    diffuse += 0.2 * diffColor * max(0.0, dot(normal, lightDir2));
+    vec3 diffuse = diffColor.xyz * max(0.0, dot(normal, lightDir1));
+    diffuse += 0.2 * diffColor.xyz * max(0.0, dot(normal, lightDir2));
 
     gl_FragColor = vec4(diffuse, 1.0);
 
